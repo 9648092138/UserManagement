73ebb3093acad392a79fb3e4aca82dff7fa552af
Om Prakash <om.prakash@ftechiz.com>
2020-05-19 10:14:35 +0530

    user management

---
+++ src/main/java/com/op/eureka/userManegement/UserAccountManagementApplication.java
@@ -0,0 +1,15 @@
+package com.op.eureka.userManegement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class UserAccountManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserAccountManagementApplication.class, args);
+	}
+
+}
